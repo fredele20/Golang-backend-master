4e6d45a6a2fb00a1494f97179526783327151d60
Fredel <48722410+fredele20@users.noreply.github.com>
2023-02-17 09:05:11 +0000

    implementation of environment variables with viper

--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import (
 	"database/sql"
 	"golang-backend-structure/api"
 	db "golang-backend-structure/db/sqlc"
+	"golang-backend-structure/util"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmodel=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
