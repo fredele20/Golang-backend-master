47750d1e59e4ec2ae67b25bc2a5991b95e088661
Fredel <48722410+fredele20@users.noreply.github.com>
2023-02-17 08:32:03 +0000

    added REST HTTP implementation

---
+++ api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	db "golang-backend-structure/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves all HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to router
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
